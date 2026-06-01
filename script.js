1f8f7f5e0e1d7ac18b0aa8adf20aabee55cb0bfe
Semuraii <208602227+Semuraii@users.noreply.github.com>
2026-06-01 12:31:51 +0200

    chnaged filter back

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ const products = [
   console.log("productNames:", productNames);
 
   const electronicsProducts = products.filter(function (product) {
-    return product.category === "home appliances" || product.category === "clothing";
+    return product.category === "electronics";
   });
   console.log("electronicsProducts:", electronicsProducts);
